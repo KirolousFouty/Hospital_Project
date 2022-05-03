371bd27cf1cff2864963bf327a95b67a8c70a581
FarahWahib <farah-samer@aucegypt.edu>
2022-05-03 20:31:46 +0200

    Added pharmacy.h

---
+++ Pharmacy.h
@@ -0,0 +1,23 @@
+#ifndef PHARMACY_H
+#define PHARMACY_H
+
+#include <iostream>
+#include <QString>
+
+using namespace std;
+
+class Pharmacy
+{
+private:
+Qstring assigned_medicine_from_doctor
+
+public:
+Pharmacy();
+Pharmacy(Qstring M);
+void setMed(Qstring M);
+Qstring getMed();
+
+Qstring getMedicine_fromDoctor();
+}
+
+#endif
