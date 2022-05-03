61fecbfbc7e6b5c7ba9a951615977faafbaf86a9
FarahWahib <farah-samer@aucegypt.edu>
2022-05-03 20:56:25 +0200

    Added Drugs.h

---
+++ Drugs.h
@@ -0,0 +1,22 @@
+#ifndef DRUGS_H
+#define DRUGS_H
+
+#include <iostream>
+
+using namespace std;
+
+class Drugs
+{
+private:
+double price;
+
+public:
+Drugs();
+Drugs(double P);
+void setPrice(double P);
+
+double getPrice();
+void Purchase();
+}
+
+#endif
